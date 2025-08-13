025742d475a124b6bf13355d1647579026a6c090
6ajmon <6ajmon@gmail.com>
2025-08-13 08:00:31 +0200

    Add damage numbers and use components in hamster and planks

--- Source/Components/AttackComponent/AttackComponent.cs
+++ Source/Components/AttackComponent/AttackComponent.cs
@@ -2,13 +2,13 @@ using Godot;
 public struct Attack
 {
     public float Damage;
-    public Vector3 Direction;
-    public Node3D Source;
+    public Vector3 GlobalPosition;
+    public float SpeedForce;
 
-    public Attack(float damage, Vector3 direction, Node3D source)
+    public Attack(float damage, Vector3 globalPosition, float speedForce)
     {
         Damage = damage;
-        Direction = direction;
-        Source = source;
+        GlobalPosition = globalPosition;
+        SpeedForce = speedForce;
     }
 }
--- Source/Components/HealthComponent/HealthComponent.cs
+++ Source/Components/HealthComponent/HealthComponent.cs
@@ -3,4 +3,25 @@ using System;
 
 public partial class HealthComponent : Node
 {
+    [Export] public float MaxHealth { get; set; } = 100;
+    [Export] public DamageNumberDisplayComponent DamageNumberDisplay { get; set; }
+    private float _currentHealth;
+
+    public override void _Ready()
+    {
+        _currentHealth = MaxHealth;
+    }
+
+    public void DealDamage(Attack attack)
+    {
+        if (attack.SpeedForce < 25)
+        {
+            // Ignore low-speed attacks
+            return;
+        }
+        float damageToDeal = attack.Damage * attack.SpeedForce;
+        _currentHealth -= damageToDeal;
+
+        DamageNumberDisplay?.ShowDamage(damageToDeal, attack.GlobalPosition);
+    }
 }
